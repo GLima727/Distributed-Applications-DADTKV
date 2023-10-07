becb68a812a978c2969888a15e7f18f9cbe362f7
Guilherme Lima <82712415+GLima727@users.noreply.github.com>
2023-10-07 12:45:12 +0100

    first commit

--- Initializer/Program.cs
+++ Initializer/Program.cs
@@ -1,2 +1,170 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System.Diagnostics;
+
+namespace DADTKV.initializer
+{
+    class Initializer
+    {
+
+        static string getCurrrentPath()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory().ToString();
+
+            string[] directorys;
+            char sepr;
+            if (currentDirectory.Contains('\\'))
+            {
+                sepr = '\\';
+                directorys = currentDirectory.Split('\\');
+            }
+            else
+            {
+                sepr = '/';
+                directorys = currentDirectory.Split('/');
+            }
+
+            string dirHead = "";
+            foreach (string directory in directorys)
+            {
+                dirHead += directory + sepr;
+                if (directory.Equals("DadTkvProject"))
+                    break;
+            }
+
+            return dirHead;
+
+        }
+
+        public static string getLeasersList(List<string> lines)
+        {
+            string leaderList = "";
+            foreach (string line in lines)
+            {
+                string[] components = line.Split(' ');
+                string command = components[0];
+                if (command == "P")
+                {
+                    string processId = components[1];
+                    string processType = components[2];
+                    string processArg = components[3];
+                    if (processType == "L")
+                        leaderList += processId + " " + processArg + " ";
+                }
+            }
+
+            return leaderList;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            string filePath = args[0];
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Error: Missing arguments.");
+                Console.WriteLine("Usage: Initializer.exe <PathToSystemConfigurationFile>");
+                return;
+            }
+
+            OperatingSystem os = Environment.OSVersion;
+
+            Console.WriteLine("Initializing...");
+
+            //VARIABLES//
+            string projectPath = getCurrrentPath();
+            List<string> file_lines = new List<string>(File.ReadAllLines(filePath));
+            string LleaderList = getLeasersList(file_lines);
+            //-------------------------------------------------------------------------//
+            
+
+
+
+
+            foreach (string line in file_lines)
+            {
+                string[] components = line.Split(' ');
+                string command = components[0];
+                switch (command)
+                {
+                    case "P":
+                        string processId = components[1];
+                        string processType = components[2];
+                        string processArg = components[3];
+                        ProcessStartInfo startInfo;
+
+                        switch (processType)
+                        {
+                            case "T":
+                                startInfo = new ProcessStartInfo();
+                                switch (os.Platform)
+                                {
+                                    case PlatformID.Win32NT:
+                                        startInfo.FileName = "cmd.exe";
+                                        startInfo.Arguments =
+                                            $"/k cd \"{projectPath + "\\TransactionManager"}\"" +
+                                            " && " +
+                                            $"dotnet run -- \"{processId}\" \"{processArg}\"";
+                                        startInfo.UseShellExecute = true;
+                                        break;
+                                    case PlatformID.Unix:
+                                        startInfo.FileName = "/bin/bash";
+                                        startInfo.Arguments =
+                                            $"-c \"kgx -e 'cd {projectPath + "TransactionManager"}" +
+                                            " && " +
+                                            $" dotnet run {processId} {processArg}'\"";
+                                        Thread.Sleep(20);
+                                        break;
+                                }
+                                Process.Start(startInfo);
+                                break;
+                            case "L":
+                                startInfo = new ProcessStartInfo();
+                                switch (os.Platform)
+                                {
+                                    case PlatformID.Win32NT:
+                                        startInfo.FileName = "cmd.exe";
+                                        startInfo.Arguments =
+                                            $"/k cd \"{projectPath + "\\LeaseManager"}\"" +
+                                            " && " +
+                                            $"dotnet run -- \"{processId}\" \"{processArg}\" \"{LleaderList}\"";
+                                        startInfo.UseShellExecute = true;
+                                        break;
+                                    case PlatformID.Unix:
+                                        startInfo.FileName = "/bin/bash";
+                                        startInfo.Arguments =
+                                            $"-c \"kgx -e 'cd {projectPath + "LeaseManager"}" +
+                                            " && " +
+                                            $" dotnet run {processId} {processArg} {LleaderList}'\"";
+                                        Thread.Sleep(200);
+                                        break;
+                                }
+                                Process.Start(startInfo);
+                                break;
+                            case "C":
+                                startInfo = new ProcessStartInfo();
+                                switch (os.Platform)
+                                {
+                                    case PlatformID.Win32NT:
+                                        startInfo.FileName = "cmd.exe";
+                                        startInfo.Arguments =
+                                            $"/k cd \"{projectPath + "\\Client"}\"" +
+                                            " && " +
+                                            $"dotnet run -- \"{processId}\" \"{projectPath + "/Initializer/scripts/" + processArg}\"";
+                                        startInfo.UseShellExecute = true;
+                                        break;
+                                    case PlatformID.Unix:
+                                        startInfo.FileName = "/bin/bash";
+                                        break;
+                                }
+                                Process.Start(startInfo);
+                                break;
+                        }
+                        break;
+                }
+            }
+
+        }
+    }
+}
--- TransactionManager/Program.cs
+++ TransactionManager/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Core;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Error: Missing arguments.");
+            Console.WriteLine("Usage: TransactionManager.exe <TransactionManagerID> <Url>");
+            return;
+        }
+        //System.Diagnostics.Debugger.Launch();
+
+        string id = args[0];
+        string processUrl = args[1];
+
+        Uri uri = new Uri(processUrl);
+
+        int port = uri.Port;
+        string hostname = uri.Host;
+
+        string startupMessage = "Insecure TransactionManager server listening on port " + port;
+
+        Console.WriteLine(startupMessage);
+
+        ServerPort serverPort = new ServerPort(hostname, port, ServerCredentials.Insecure);
+
+        AppContext.SetSwitch(
+            "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+        Server server = new Server
+        {
+            Services = { },
+            Ports = { serverPort }
+        };
+
+        server.Start();
+
+        while (true)
+        {
+
+        }
+
+
+    }
+}
+
